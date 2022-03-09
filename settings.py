d2d4ca1ca6823d753409355d891c03d851f1c149
Артем Гронский <ares.crtc@gmail.com>
2022-03-09 16:33:05 +0300

    review#1

--- settings.py
+++ settings.py
@@ -1,9 +1,9 @@
 """Константы для работы телеграм бота."""
-RETRY_TIME = 60
+RETRY_TIME = 600
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
 HOMEWORK_STATUSES = {
     'approved': 'Работа проверена: ревьюеру всё понравилось. Ура!',
     'reviewing': 'Работа взята на проверку ревьюером.',
     'rejected': 'Работа проверена: у ревьюера есть замечания.'
 }
-START_DATE = 2022, 3, 1
+START_DATE = [2022, 3, 1]
